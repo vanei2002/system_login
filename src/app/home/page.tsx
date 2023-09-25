a245e3ac0a0c7fc48d9d65c63be849e322593587
vanei2002 <vanei.jesus016@gmail.com>
2023-09-24 22:37:06 -0300

    iniciando projeto

---
+++ src/app/home/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Homer() {
+
+    
+    return (
+      <h2>Funcionando de mais</h2>
+    )
+  }
+  
---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+
+export default function AuthUser() {
+  return (
+    <h2>Funcionando</h2>
+  )
+}

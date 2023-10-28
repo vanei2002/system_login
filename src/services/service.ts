0c4828853085b4b80df12f36bdbbe1a1cd902570
vanei2002 <vanei.jesus016@gmail.com>
2023-10-28 12:22:48 -0300

    example env

--- src/services/service.ts
+++ src/services/service.ts
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const connectService = axios.create({
-  baseURL: process.env.BASE_URL, // Substitua pela URL da sua API
+  baseURL: process.env.NEXT_PUBLIC_SERVER, // Substitua pela URL da sua API
   headers: {
     'Content-Type': 'application/json',
     // Outros cabeçalhos de autenticação, se necessário
